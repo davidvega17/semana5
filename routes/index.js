cf7a5fe9572c85539193922008697877e5851560
Mateo Vega <davidvegat17@gmail.com>
2020-12-19 18:42:23 -0500

    routes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 const routerx = require('express-promise-router');
-const categoriaRouter = require('./articulo');
-
-
+const articuloRouter = require('./articulo.js');
+const userRouter = require('./user.js');
+const categoriaRouter = require('./categoria.js');
 
 const router = routerx();
 
 router.use('/articulo', articuloRouter);
-
+router.use('/usuario', userRouter);
+router.use('/categoria', categoriaRouter);
 module.exports = router;
--- services/token.js
+++ services/token.js
@@ -1,17 +1,37 @@
 var jwt = require('jsonwebtoken');
-const models = require('../models');
-
+const db = require('../models');
+const key = require('../secret/config.js')
 
 module.exports = {
 
     //generar el token
-    encode: async(id, rol) => {
-
+    encode: async(user) => {
+        var token = jwt.sign(
+            {   id: user.id,
+                name: user.name,
+                email: user.email,
+                rol: user.rol,
+                status: user.estado
+            },
+            key.secret,
+            {
+            expiresIn: 86400
+        });
+        return token;
     },
     //permite decodificar el token
     decode: async(token) => {
         try {
-
+            const {id} = await jwt.verify(token, key.secret);
+            const user = await db.Usuario.findOne({where:{
+                id: id,
+                estado: 1
+            }});
+            if(user){
+                return user;
+            }else{
+                return false;
+            }
         } catch (e) {
 
         }
